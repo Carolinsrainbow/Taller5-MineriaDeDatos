8ab02c482e9684fad53af6b66206371143c84df7
CarolinsRainbow <60187454+Carolinsrainbow@users.noreply.github.com>
2021-07-19 20:02:15 -0400

    Incorporación de paquetes

--- Taller 5 MDD - Herrera Azolas Carolina.R
+++ Taller 5 MDD - Herrera Azolas Carolina.R
@@ -1,4 +1,17 @@
 ####Taller 5
 
 ## Incorporación de los datos a la biblioteca 
+
 datos <- read.csv("/Users/user/Desktop/SouthGermanCredit.csv",sep=";", header=TRUE)
+
+# Instalamos los paquetes necesarios, en caso que no los tengamos instalados
+
+install.packages("cluster")
+install.packages("factoextra")
+install.packages("tidyverse")
+
+# Cargamos las librerias que utilizaremos
+
+library(cluster)
+library(factoextra)
+library(tidyverse)
