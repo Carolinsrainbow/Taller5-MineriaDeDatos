46b76602cf09ac1bf093c9ca398fb004d3a345af
CarolinsRainbow <60187454+Carolinsrainbow@users.noreply.github.com>
2021-07-25 01:58:13 -0400

    Add files via upload

--- Taller 5 MDD - Herrera Azolas Carolina.R
+++ Taller 5 MDD - Herrera Azolas Carolina.R
@@ -31,21 +31,21 @@ install.packages("class")
 # Cargamos las librerias que utilizaremos
 library(class)
 
-
 ######### PRIMER CASO
 
 ## Elegimos las variables para el análisis y las que son categóricas se normalizan
 
-datos_adaptados <- as.data.frame(cbind(age = datos$age,
-                                       historial_delay = (datos$history == 0),
-                                       historial_critic = (datos$history == 1),
-                                       historial_new =(datos$history ==2),
-                                       historial_clean = (datos$history == 3),
-                                       savings = datos$savings,
-                                       employed = datos$employed,
-                                       job = datos$job,
-                                       foreign = datos$foreign,
-                                       credit = datos$credit))
+datos_adaptados <- as.data.frame(cbind(
+  historial_delay = (datos$history == 0),
+  historial_critic = (datos$history == 1),
+  historial_new =(datos$history ==2),
+  historial_clean = (datos$history == 3),
+  savings = datos$savings,
+  employed = datos$employed,
+  job = datos$job,
+  foreign = datos$foreign,
+  credit = datos$credit,
+  persons=datos$persons))
 # Normalizamos los datos
 normalize <- function(x) { return ((x - min(x)) / (max(x) - min(x))) }
 
@@ -67,7 +67,7 @@ plot(clusters, codo, type = "l")
 ##Obtemos K=2! #Let'sDoIt
 
 ## Aplicamos silueta
-silueta <- c(0,0,0,0,0,0,0,0,0)
+silueta <- c(0,0,0,0,0,0,0,0,0,0)
 for (k in 2:10) { 
   modelo_aux <- kmeans(datos_norm1,
                        centers = k,
@@ -79,16 +79,44 @@ for (k in 2:10) {
 ## Gráficamos silueta
 plot(clusters, silueta, type = "l")
 
-## Max 9, menos clusters mqs robusto, mejor interpretación y consistencia
+## 10 es el peak pero no se respalda con el gráfico anterior
 
-# K Means con K = 9
+# K Means con K = 10
 
-k9 <- kmeans(datos_norm1,
-             centers = 4,
+k10 <- kmeans(datos_norm1,
+              centers = 10,
+              nstart = 25)
+
+## Agregamos la columna cluster a la tabla original de variables elegidas
+datos_adaptados$cluster <- k10$cluster
+
+
+view(datos_adaptados
+     %>% group_by(cluster)
+     %>% summarise_all(mean))
+
+# K Means con K = 6
+
+k6 <- kmeans(datos_norm1,
+             centers = 6,
              nstart = 25)
 
 ## Agregamos la columna cluster a la tabla original de variables elegidas
-datos_adaptados$cluster <- k9$cluster
+datos_adaptados$cluster <- k6$cluster
+
+
+view(datos_adaptados
+     %>% group_by(cluster)
+     %>% summarise_all(mean))
+
+# K Means con K = 2
+
+k2 <- kmeans(datos_norm1,
+             centers = 2,
+             nstart = 25)
+
+## Agregamos la columna cluster a la tabla original de variables elegidas
+datos_adaptados$cluster <- k2$cluster
 
 
 view(datos_adaptados
@@ -96,25 +124,22 @@ view(datos_adaptados
      %>% summarise_all(mean))
 
 
-####### SEGUNDO CASO 
 
-## Elegimos las variables para el análisis y las que son categóricas se normalizan
+##### Caso 2
 
-datos_adaptados2 <- as.data.frame(cbind(age = datos$age,
-                                       savings = datos$savings,
+datos_adaptados2 <- as.data.frame(cbind(savings = datos$savings,
                                        employed = datos$employed,
                                        job = datos$job,
                                        foreign = datos$foreign,
                                        credit = datos$credit))
+
 # Normalizamos los datos
 normalize <- function(x) { return ((x - min(x)) / (max(x) - min(x))) }
-
-## datos_norm <- as.data.frame(lapply(datos_small, normalize))
-datos_norm2 <- as.data.frame(lapply(datos_adaptados2, normalize))
+datos_norm2 <- as.data.frame(lapply(datos_adaptados, normalize))
 
 ## Elegimos el K
 
-clusters <- c(1,2,3,4,5,6,7,8,9,10)
+clusters2 <- c(1,2,3,4,5,6,7,8,9,10)
 codo <- c(0,0,0,0,0,0,0,0,0,0)
 for (k in 1:10) { 
   codo[k] <- kmeans(datos_norm2,
@@ -124,33 +149,20 @@ for (k in 1:10) {
 
 plot(clusters, codo, type = "l")
 
-##Obtemos K=2! #Let'sDoIt
+## K=2
+
 
 ## Aplicamos silueta
-silueta2 <- c(0,0,0,0,0,0,0,0,0)
+silueta2 <- c(0,0,0,0,0,0,0,0,0,0)
 for (k in 2:10) { 
-  modelo_aux <- kmeans(datos_norm2,
+  modelo_aux2 <- kmeans(datos_norm2,
                        centers = k,
                        nstart = 25)
-  silueta_aux <- silhouette(modelo_aux$cluster, dist(datos_norm2))
-  silueta[k] <- mean(silueta_aux[, 3])
+  silueta_aux2 <- silhouette(modelo_aux2$cluster, dist(datos_norm2))
+  silueta[k] <- mean(silueta_aux2[, 3])
 }
 
 ## Gráficamos silueta
-plot(clusters, silueta2, type = "l")
-
-## Max 9, menos clusters mqs robusto, mejor interpretación y consistencia
-
-# K Means con K = 9
-
-k9 <- kmeans(datos_norm1,
-             centers = 4,
-             nstart = 25)
-
-## Agregamos la columna cluster a la tabla original de variables elegidas
-datos_adaptados$cluster <- k9$cluster
+plot(clusters2, silueta2, type = "l")
 
 
-view(datos_adaptados
-     %>% group_by(cluster)
-     %>% summarise_all(mean))
