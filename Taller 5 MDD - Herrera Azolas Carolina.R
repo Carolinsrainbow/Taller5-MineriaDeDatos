3ea910e4bb2b5689744821eb7a7580379aea63ce
CarolinsRainbow <60187454+Carolinsrainbow@users.noreply.github.com>
2021-07-25 00:42:32 -0400

    Add files via upload

--- Taller 5 MDD - Herrera Azolas Carolina.R
+++ Taller 5 MDD - Herrera Azolas Carolina.R
@@ -16,79 +16,141 @@ library(cluster)
 library(factoextra)
 library(tidyverse)
 
-## Normalizamos los datos
+#Visualización general 
+head(datos)
+
+## Resumen estadístico
+summary(datos)
+
+## Cantidad de datos 
+length(datos$personal)
+
+# Instalamos los paquetes necesarios, en caso que no los tengamos instaladas
+install.packages("class")
+
+# Cargamos las librerias que utilizaremos
+library(class)
+
+
+######### PRIMER CASO
+
+## Elegimos las variables para el análisis y las que son categóricas se normalizan
+
+datos_adaptados <- as.data.frame(cbind(age = datos$age,
+                                       historial_delay = (datos$history == 0),
+                                       historial_critic = (datos$history == 1),
+                                       historial_new =(datos$history ==2),
+                                       historial_clean = (datos$history == 3),
+                                       savings = datos$savings,
+                                       employed = datos$employed,
+                                       job = datos$job,
+                                       foreign = datos$foreign,
+                                       credit = datos$credit))
+# Normalizamos los datos
 normalize <- function(x) { return ((x - min(x)) / (max(x) - min(x))) }
 
-datos_norm <- as.data.frame(lapply(datos, normalize))
+## datos_norm <- as.data.frame(lapply(datos_small, normalize))
+datos_norm1 <- as.data.frame(lapply(datos_adaptados, normalize))
 
-# K Means con K = 15
-k3 <- kmeans(datos_norm,
-             centers = 3,
-             nstart = 25)
+## Elegimos el K
 
-# Analicemos los resultados
-
-# Tamano de cada cluster
-k3$size
-# Cluster al que pertenece cada observacion
-k3$cluster
-# Centros de cada cluster
-k3$centers
-# Suma cuadratica inicial de distancias
-k3$totss
-# Sumas cuadraticas de distancias dentro de cada cluster
-k3$withinss
-k3$tot.withinss
-
-
-# Apliquemos el codo
 clusters <- c(1,2,3,4,5,6,7,8,9,10)
 codo <- c(0,0,0,0,0,0,0,0,0,0)
 for (k in 1:10) { 
-  codo[k] <- kmeans(datos_norm,
+  codo[k] <- kmeans(datos_norm1,
                     centers = k,
                     nstart = 25)$tot.withinss
 }
 
 plot(clusters, codo, type = "l")
 
+##Obtemos K=2! #Let'sDoIt
 
-# Calculemos los valores de silueta para nuestros clusters
-silueta_k3 <- silhouette(k3$cluster, dist(datos_norm))
-summary(silueta_k3)
-
-# La primera columna es el cluster de cada observacion
-silueta_k3[,1]
-# La segunda columna es el cluster vecino de cada observacion
-silueta_k3[,2]
-# La tercera columna es el valor de silueta de cada observacion
-silueta_k3[,3]
-
-# Grafiquemos la silueta
+## Aplicamos silueta
 silueta <- c(0,0,0,0,0,0,0,0,0)
 for (k in 2:10) { 
-  modelo_aux <- kmeans(datos_norm,
+  modelo_aux <- kmeans(datos_norm1,
                        centers = k,
                        nstart = 25)
-  silueta_aux <- silhouette(modelo_aux$cluster, dist(datos_norm))
+  silueta_aux <- silhouette(modelo_aux$cluster, dist(datos_norm1))
   silueta[k] <- mean(silueta_aux[, 3])
 }
 
+## Gráficamos silueta
 plot(clusters, silueta, type = "l")
 
-# Distribuciones de valores de silueta
-fviz_silhouette(silueta_k3)
+## Max 9, menos clusters mqs robusto, mejor interpretación y consistencia
+
+# K Means con K = 9
+
+k9 <- kmeans(datos_norm1,
+             centers = 4,
+             nstart = 25)
+
+## Agregamos la columna cluster a la tabla original de variables elegidas
+datos_adaptados$cluster <- k9$cluster
 
 
-# Interpretemos que es cada cluster
+view(datos_adaptados
+     %>% group_by(cluster)
+     %>% summarise_all(mean))
 
-# Agreguemos el cluster de cada observacion a los datos
-datos$cluster3 <- k3$cluster
 
-# Podemos analizar visualmente los clusteres
-plot(datos$eruptions, datos$waiting, col = datos$cluster3)
+####### SEGUNDO CASO 
 
-# Podemos analizar las variables promedio de cada cluster
-View(datos
-     %>% group_by(cluster3)
+## Elegimos las variables para el análisis y las que son categóricas se normalizan
+
+datos_adaptados2 <- as.data.frame(cbind(age = datos$age,
+                                       savings = datos$savings,
+                                       employed = datos$employed,
+                                       job = datos$job,
+                                       foreign = datos$foreign,
+                                       credit = datos$credit))
+# Normalizamos los datos
+normalize <- function(x) { return ((x - min(x)) / (max(x) - min(x))) }
+
+## datos_norm <- as.data.frame(lapply(datos_small, normalize))
+datos_norm2 <- as.data.frame(lapply(datos_adaptados2, normalize))
+
+## Elegimos el K
+
+clusters <- c(1,2,3,4,5,6,7,8,9,10)
+codo <- c(0,0,0,0,0,0,0,0,0,0)
+for (k in 1:10) { 
+  codo[k] <- kmeans(datos_norm2,
+                    centers = k,
+                    nstart = 25)$tot.withinss
+}
+
+plot(clusters, codo, type = "l")
+
+##Obtemos K=2! #Let'sDoIt
+
+## Aplicamos silueta
+silueta2 <- c(0,0,0,0,0,0,0,0,0)
+for (k in 2:10) { 
+  modelo_aux <- kmeans(datos_norm2,
+                       centers = k,
+                       nstart = 25)
+  silueta_aux <- silhouette(modelo_aux$cluster, dist(datos_norm2))
+  silueta[k] <- mean(silueta_aux[, 3])
+}
+
+## Gráficamos silueta
+plot(clusters, silueta2, type = "l")
+
+## Max 9, menos clusters mqs robusto, mejor interpretación y consistencia
+
+# K Means con K = 9
+
+k9 <- kmeans(datos_norm1,
+             centers = 4,
+             nstart = 25)
+
+## Agregamos la columna cluster a la tabla original de variables elegidas
+datos_adaptados$cluster <- k9$cluster
+
+
+view(datos_adaptados
+     %>% group_by(cluster)
      %>% summarise_all(mean))
